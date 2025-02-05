0f31d4b7388e4ca9e47e89f3fc2df012749b26df
Belayne <37399395+Belayne@users.noreply.github.com>
2025-02-05 14:06:16 +0100

    inital commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express, { urlencoded } from "express";
+import path from "path";
+
+const PORT = process.env.PORT || 8000;
+const __dirname = import.meta.dirname;
+const assetsPath = path.join(__dirname, "public");
+const app = express();
+
+app.use(express.static(assetsPath));
+
+app.use(urlencoded({extended: true}));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
